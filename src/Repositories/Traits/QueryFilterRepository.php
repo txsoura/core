a072847a58307de1cda1bd44e3965f246c97476d
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-05-09 15:37:08 -0300

    feat(repository): create base query filter repository

---
+++ src/Repositories/Traits/QueryFilterRepository.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Txsoura\Core\Repositories\Traits;
+
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+
+trait QueryFilterRepository
+{
+    public function checkDateColumns()
+    {
+        if (in_array($this->request['date_column'], $this->allow_between_dates)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function checkValueColumns()
+    {
+        if (in_array($this->request['value_column'], $this->allow_between_values)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function checkQueryColumns()
+    {
+        if (in_array($this->request['q'], $this->allow_like)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function checkSortColumns($columns)
+    {
+        foreach ($columns as $column) {
+            if (Str::contains($column, '-')) {
+                $column = substr($column, 1);
+            }
+
+            if (!in_array($column, $this->allow_order)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public function checkIncludeColumns()
+    {
+        $columns = explode(',', $this->request['include']);
+
+        foreach ($columns as $column) {
+
+            if (!in_array($column, $this->possibleRelationships)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public function checkWhereColumns()
+    {
+        $columns = [];
+
+        foreach (array_keys($this->request) as $column) {
+            if (in_array($column, $this->allow_where)) {
+                Arr::set($columns, $column, $this->request[$column]);
+            }
+        }
+
+        return $columns;
+    }
+}
+
