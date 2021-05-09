ba0ca7c239d51f4d1759c5690705513d7436050d
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-05-09 15:37:33 -0300

    feat(lang): create en dictionary

---
+++ src/resources/lang/en/message.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'route_not_found' => 'Route not found',
+    'entry_not_found' => 'Entry for :model not found',
+    'relation_not_found' => 'Relation not found',
+    'not_found' => 'Not found',
+    'deleted' => 'Deleted successfully',
+    'restored' => 'Restored successfully',
+    'invalid_signature' => 'Invalid signature',
+];
