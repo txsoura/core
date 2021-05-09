19b2d1de5199e2f2bc678402639c110864c90907
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-05-09 15:33:53 -0300

    feat(middleware): create response json

---
+++ src/Http/Middleware/ResponseJson.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Txsoura\Core\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ResponseJson
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $request->headers->set('Accept', 'application/json');
+
+        return $next($request);
+    }
+}
