35ee0cb7a1814515628b6046b04448dcc4d681c4
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-05-09 15:37:56 -0300

    feat(lang): create pt dictionary

---
+++ src/resources/lang/pt/message.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'route_not_found' => 'Rota não encontrada',
+    'entry_not_found' => 'Entrada para :model não encontrado',
+    'relation_not_found' => 'Relação não encontrada',
+    'not_found' => 'Não encontrado',
+    'deleted' => 'Apagado com successo',
+    'restored' => 'Restaurado com successo',
+    'invalid_signature' => 'Assinatura inválida',
+];
