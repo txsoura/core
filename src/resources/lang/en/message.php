6cd2b17166b8743de5e1f518398903055801ddb5
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-06-15 01:18:48 -0300

    Feat/vic add eloquent status messages (#4)
    * feat(controller): add CRUD eloquent status check
    
    * feat(controller): add soft delete eloquent status check
    
    * feat(service): add CRUD eloquent status check
    
    * feat(service): add CRUD eloquent status check
    
    * feat(lang): update en message dictionary
    
    * feat(lang): update es message dictionary
    
    * feat(lang): update pt message dictionary
    
    * release 7.0.1

--- src/resources/lang/en/message.php
+++ src/resources/lang/en/message.php
@@ -5,7 +5,13 @@ return [
     'entry_not_found' => 'Entry for :model not found',
     'relation_not_found' => 'Relation not found',
     'not_found' => 'Not found',
+    'stored' => 'Stored successfully',
+    'updated' => 'Updated successfully',
     'deleted' => 'Deleted successfully',
     'restored' => 'Restored successfully',
+    'store_failed' => 'Store failed',
+    'update_failed' => 'Update failed',
+    'delete_failed' => 'Delete failed',
+    'restore_failed' => 'Restore failed',
     'invalid_signature' => 'Invalid signature',
 ];
