f6ce0f30f0f28d9dbe7b28172cdb766f1955aa73
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-06-15 01:11:46 -0300

    feat(lang): update pt message dictionary

--- src/resources/lang/pt/message.php
+++ src/resources/lang/pt/message.php
@@ -5,7 +5,13 @@ return [
     'entry_not_found' => 'Entrada para :model não encontrado',
     'relation_not_found' => 'Relação não encontrada',
     'not_found' => 'Não encontrado',
+    'stored' => 'Armazenado com sucesso',
+    'updated' => 'Atualizado com sucesso',
     'deleted' => 'Apagado com successo',
     'restored' => 'Restaurado com successo',
+    'store_failed' => 'Armazenado falhou',
+    'update_failed' => 'Atualização falhou',
+    'delete_failed' => 'Exclusão falhou',
+    'restore_failed' => 'Restauração falhou',
     'invalid_signature' => 'Assinatura inválida',
 ];
