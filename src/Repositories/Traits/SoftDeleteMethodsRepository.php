6b3879d83c7eba9e94bef48bf0e5864417312300
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-07-27 21:14:48 -0300

    feat(repository): create soft delete trait methods

---
+++ src/Repositories/Traits/SoftDeleteMethodsRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Txsoura\Core\Services\Traits;
+
+use Illuminate\Support\Facades\Log;
+
+trait SoftDeleteMethodsRepository
+{
+    /**
+     * @param int $id
+     * @return Model|null
+     */
+    public function findWithTrased($id)
+    {
+        return $this->model()::withTrashed()
+            ->where('id', $id)
+            ->when(key_exists('include', $this->request), function ($query) {
+                return $query->with(explode(',', $this->request['include']));
+            })
+            ->first();
+    }
+
+    /**
+     * @param int $id
+     * @return Model|null
+     */
+    public function findOrFailWithTrased($id)
+    {
+        return $this->model()::withTrashed()
+            ->where('id', $id)
+            ->when(key_exists('include', $this->request), function ($query) {
+                return $query->with(explode(',', $this->request['include']));
+            })
+            ->firstOrFail();
+    }
+}
